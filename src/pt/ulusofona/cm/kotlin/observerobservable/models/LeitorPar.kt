857614d280929f47cf0b2eea2d0e178c49d690e0
JoaoLima-21906948 <72455320+JoaoLima-21906948@users.noreply.github.com>
2022-05-15 16:21:35 +0100

    Add files via upload

---
+++ src/pt/ulusofona/cm/kotlin/observerobservable/models/LeitorPar.kt
@@ -0,0 +1,16 @@
+package pt.ulusofona.cm.kotlin.observerobservable.models
+
+class LeitorPar(nome: String, registado: Boolean) : Leitor(nome, registado) {
+    var numeros = mutableListOf<Int>()
+
+    fun onReceiveNumero(numero : Int){
+        if(numero % 2 == 0){
+            numeros.add(numero)
+        }
+    }
+
+    fun imprimeNumeros(): String{
+        return "$nome leu os seguintes numeros pares: $numeros"
+    }
+
+}
---
+++ src/pt/ulusofona/cm/kotlin/observerobservable/models/Noticia.kt
@@ -0,0 +1,5 @@
+package pt.ulusofona.cm.kotlin.observerobservable.models
+
+class Noticia(titulo : String, corpo: String) {
+
+}
